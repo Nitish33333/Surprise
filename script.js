2b03d3a24c0e05b1c806b5045b5444aaffa9a1de
Nitish33333 <s.nitishnadar@gmail.com>
2024-06-04 01:47:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+
+function closeMessage() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
